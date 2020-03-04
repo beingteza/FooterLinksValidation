a0dd46b57b0985c4aaf68692ba0e6ec54e3f841c
vangiven <vangiven@XDSFC4077.Mars-AD.Net>
2020-03-04 19:31:51 +0530

    Adding Changes

--- Test Suites/01.ts
+++ Test Suites/01.ts
@@ -11,81 +11,99 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>b63c7a6e-6e9d-4059-b5d2-2fe8ac6b6f9a</testSuiteGuid>
    <testCaseLink>
-      <guid>6d56b3d1-76bb-4951-b322-94ba9023bd89</guid>
+      <guid>7f904697-638e-46db-b928-b6880f6a37cf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Accessability</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Accessibility</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>23f4d4d9-ffff-4bf3-8c3c-b994f33de0a3</guid>
+      <guid>a6ae0c69-bb4a-495a-b009-5b64e7a698b0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/AdChoices</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Careers</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>9bb0765a-7e39-42bf-9b24-4f31a335a0ac</guid>
+      <guid>3030cc84-224c-4192-a649-1cc2869ccff1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Careers</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/ContactUsPage</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>3465aa87-97e4-4d0f-b1a5-16a73aac46b3</guid>
+      <guid>e8caa373-8b8a-4800-aa34-1a006c5df04c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/ContactUsPage</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Cookies</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>53077229-5fd0-4441-8edb-58e1163c6667</guid>
+      <guid>515b26e1-9b01-484a-b2f6-24fb00666b18</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Cookie</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Facebook</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>ff4a36ba-f2fc-4370-b75f-13c556739efa</guid>
+      <guid>a429b6f7-165c-4f4e-841a-e2b34e4242a2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Legal</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Instagram</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e38e760a-c441-4f80-9d46-8ba6d3c4c3d0</guid>
+      <guid>e30c13e4-2ada-4a13-8892-49b7c8251572</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Mars Alumni</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Legal</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e8689f4c-585e-4b5f-b317-37d72e5e92ea</guid>
+      <guid>ea333514-b2c9-41c1-bed8-d8a426e4d761</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/ModernSlaveryAct</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/LinkedIn</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e4f04eda-c457-4cfb-8e58-06ecb6471cde</guid>
+      <guid>f0323dc0-a38e-4263-a255-991eed48f28a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Privacy</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Locations</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>1b1760fc-2912-42ee-9692-e46db7b4773a</guid>
+      <guid>1111dc0f-3139-4ae3-bb53-b6a6125516ea</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/SiteMap</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/MarsAlumni</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>cd1a80aa-e300-46f0-bf3d-166a7a5ee5b1</guid>
+      <guid>1abc0ea1-5e04-4be8-880a-2c1b08bcb62f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/SocalNetwork</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/ModernSlaveryAct</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>cb0ec92d-b506-49b0-a37e-eda5d0ec3a3f</guid>
+      <guid>ee3c884e-b5b6-4acc-9622-57c019200dbf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/SupplyChain</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Privacy</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c7e92917-256f-4e72-9458-681dd5a7c995</guid>
+      <guid>fc8c19ff-4d90-4f5b-a668-0695b0ddecca</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MarsCountrySites/Mars_Argentina/Ubicaciones</testCaseId>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/SiteMap</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>329ad7c4-1b59-4438-9cd5-ea0fe83da58c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/SupplyChain</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2092254a-0294-4374-b6ce-6a5c74c41963</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Twitter</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1729234d-0798-4d0b-b61a-f21e9263541f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MarsCountrySites/Mars_Australia/Youtube</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
